1f22110326b3fa780369204ddcfd51d7f607e795
Noah Fairbairn <noahfairbairn@gmail.com>
2018-12-28 23:36:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,16 @@ end
 def array_with_two_elements
   array = ["element_one", "element_two"]
 end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+  
